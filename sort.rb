b4807af53dbda8a107c8fb6ada60139b35230d13
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-03-19 16:07:49 -0500

    Run linter and fixed formatting errors

--- sort.rb
+++ sort.rb
@@ -1,27 +1,20 @@
-
-
 def bubble_sort(arr)
-    loops = arr.length
-    i = 0
-    
+  loops = arr.length
+  i = 0
+  while i < loops - 1
+    j = 0
+    while j < loops - 1
+      if arr[j] > arr[j + 1]
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-    while i < loops - 1
-        j = 0
-
-        while j < loops - 1
-            if  arr[j] > arr[j + 1]
-                arr[j],arr[j + 1] = arr[j + 1],arr[j]
-
-                puts arr[j].to_s + arr[j + 1].to_s
-                
-            end
-            j += 1
-        end
-
-        i += 1
+        puts arr[j].to_s + arr[j + 1].to_s
+      end
+      j += 1
     end
+    i += 1
+  end
 
-    puts arr
+  puts arr
 end
 
 bubble_sort([5, 3, 1, 0, 4])
