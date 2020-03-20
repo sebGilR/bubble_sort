c31b1b3e7c01aea9b7f0c9c30d3777316197fe98
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-03-20 12:56:28 -0500

    Linter corrections applied

--- sort.rb
+++ sort.rb
@@ -4,9 +4,7 @@ def bubble_sort(arr)
   while i < loops - 1
     j = 0
     while j < loops - 1
-      if arr[j] > arr[j + 1]
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
-      end
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j] > arr[j + 1]
       j += 1
     end
     i += 1
@@ -14,23 +12,21 @@ def bubble_sort(arr)
   puts arr
 end
 
-bubble_sort([3,4,52,31,7,5])
+bubble_sort([3, 4, 52, 31, 7, 5])
 
 def bubble_sort_by(arr)
   (arr.length - 1).step(1, -1) do |i|
-    swapped=false
+    swapped = false
     (0...i).each do |j|
-      if yield(arr[j], arr[j + 1]).to_i.positive?
-        swapped = true
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
-      end
+      swapped = true if yield(arr[j], arr[j + 1]).to_i.positive?
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if yield(arr[j], arr[j + 1]).to_i.positive?
     end
-    break if !swapped
+    break unless swapped
   end
 
   puts arr
 end
 
-bubble_sort_by(["hi","hello","hey", "longstringhere"]) do |left,right|
+bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
